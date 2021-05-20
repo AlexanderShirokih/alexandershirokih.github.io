f94e3f7301898bb91e001059a93456a92695295e
alexandershirokih <alexandrshirokih@mail.ru>
2021-05-20 15:12:48 +0300

    rewrite image preview using Jquery

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,18 +1,69 @@
+/**
+ * Simulates image fetching API call.
+ */
+function fetchImages() {
+  return new Promise((resolve, _) => {
+    resolve([
+      ...(function* () {
+        for (let i = 1; i <= 15; i++)
+          yield {
+            url: `imgs/posts/pic${i}.jpg`,
+            title: `Random picture #${i}`,
+          };
+      })(),
+    ]);
+  });
+}
+
+var currentImageIndex = 0;
+
 $(() => {
   const picsContainer = $("#rand-pictures");
 
-  $.each(Array(15), (i) => {
-    const parent = $("<div/>");
+  function hide() {
+    $("#picture-viewer").hide();
+  }
 
-    parent
-      .on("click", () => parent.toggleClass("zoomed"))
-      .append(
-        $("<img/>", {
-          src: `imgs/posts/pic${i + 1}.jpg`,
-          alt: "Random picture",
-        })
-      )
-      .append($("<span/>", { text: `Random picture ${i + 1}` }))
-      .appendTo(picsContainer);
+  hide();
+
+  $(".preview-close-btn").on("click", () => hide());
+
+  fetchImages().then((imageList) => {
+    $(".image-preview-nav-btn.left").on("click", () =>
+      switchTo(currentImageIndex - 1, imageList)
+    );
+    $(".image-preview-nav-btn.right").on("click", () =>
+      switchTo(currentImageIndex + 1, imageList)
+    );
+
+    imageList.forEach((image, i, array) => {
+      $("<div/>")
+        .append(
+          $("<img/>", {
+            src: image.url,
+            alt: image.title,
+          })
+        )
+        .appendTo(picsContainer)
+        .on("click", () => switchTo(i, array));
+    });
   });
 });
+
+function switchTo(nextId, data) {
+  currentImageIndex = nextId < 0 ? data.length + nextId : nextId % data.length;
+
+  $("#picture-viewer").fadeIn();
+
+  $("#image-preview")
+    .fadeOut(0)
+    .fadeIn()
+    .attr("src", data[currentImageIndex].url)
+    .attr("alt", data[currentImageIndex].title);
+
+  $("#image-preview-label").text(
+    `Фото ${currentImageIndex + 1} из ${data.length} - ${
+      data[currentImageIndex].title
+    }`
+  );
+}
